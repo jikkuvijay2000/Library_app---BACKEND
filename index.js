daeb4aba69f4e3501d1179cb2f4aecddfa2ed267
jikkuvijay2000 <jikkuvijay21@gmail.com>
2024-05-29 15:48:15 +0530

    changes

--- index.js
+++ index.js
@@ -9,6 +9,28 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+const allowedOrigins = [
+    'https://ornate-squirrel-ecb943.netlify.app/'
+    // Add more origins if necessary
+];
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        // Check if the origin is in the allowed origins list
+        if (allowedOrigins.includes(origin) || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true // If you need to send cookies or authorization headers
+};
+
+// Use the CORS middleware
+app.use(cors(corsOptions));
+
+
+
 app.use('/api',userRouter);
 app.use('/file',booksRouter)
 
