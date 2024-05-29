ae733863f18bbe93c9bd24e945f4d9430f959bc0
jikkuvijay2000 <jikkuvijay21@gmail.com>
2024-05-29 16:20:24 +0530

    changes

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cors());
 
 const allowedOrigins = [
-    'https://incandescent-bublanina-a28cdb.netlify.app/'
+    'https://incandescent-bublanina-a28cdb.netlify.app'
     // Add more origins if necessary
 ];
 
